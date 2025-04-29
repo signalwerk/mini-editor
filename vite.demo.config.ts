2f908b6267fc6fd28bf34a60c6cd78f8c771eb71
signalwerk <sh@signalwerk.ch>
2025-04-29 02:20:01 +0200

    ADD: Demopage on GitHub

---
+++ vite.demo.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// Special config for the demo build (GitHub Pages)
+export default defineConfig({
+  plugins: [react()],
+  // Use base path for GitHub Pages
+  base: process.env.NODE_ENV === "production" ? "/miniEditor/" : "/",
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+    sourcemap: true,
+  },
+});
